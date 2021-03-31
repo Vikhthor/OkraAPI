731aa89269d68fed2fc3d546fc9307cd708e67d9
vikhthor <victormaduforo@gmail.com>
2021-03-31 08:59:55 +0100

    dynamic document rename

--- okra_pay/okra_pay/doctype/okra_webhook/okra_webhook.py
+++ okra_pay/okra_pay/doctype/okra_webhook/okra_webhook.py
@@ -3,8 +3,17 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+# from frappe.model.naming import getseries
+import frappe.model.rename_doc as rd
 
 class OkraWebhook(Document):
-	pass
+	def autoname(self):
+		if self.name1:
+			self.name = self.name1
+		else:
+			self.name = self.customer_id
+	def validate(self):
+		if self.name1:
+			rd.rename_doc(self.doctype, self.name, self.name1, force=True, merge=False, ignore_permissions=False, ignore_if_exists=False)
